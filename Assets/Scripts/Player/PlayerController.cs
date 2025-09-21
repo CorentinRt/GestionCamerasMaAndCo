620d6f7618e4f95400d8ac91c60d1e502c592c28
CorentinRmt <corentin.remot@gmail.com>
2025-09-21 21:07:10 +0200

    [ADD] variant lock dir for player controller using camera direction

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public CONTROLS controls = CONTROLS.CAMERA;
 
+    [SerializeField] private bool _useLockDirectionByCameraOnSwitch = true;
+    [SerializeField] private FreeFollowView freeFollowView;
+
     Rigidbody _rigidbody = null;
+
+    private bool _canChangeReferenceDirection = true;
+    private Vector3 _right = Vector3.right;
+    private Vector3 _forward = Vector3.forward;
+
     protected bool IsActive { get; private set; }
 
     public void Awake()
@@ -22,38 +30,49 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 direction = Vector3.zero;
-        Vector3 forward = Vector3.forward;
-        Vector3 right = Vector3.right;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (_useLockDirectionByCameraOnSwitch && (freeFollowView.Weight > 0f || (!_canChangeReferenceDirection && horizontalInput == 0f && verticalInput == 0f)))
+        {
+            _canChangeReferenceDirection = true;
+        }
 
         switch (controls)
         {
             case CONTROLS.CAMERA:
-                Vector3 camForward = Camera.main.transform.forward;
-                Vector3 camRight = Camera.main.transform.right;
-
-                camForward.y = 0;
-                camRight.y = 0;
-
-                if (camForward.sqrMagnitude < 0.01f || camRight.sqrMagnitude < 0.01f)
+                if (_canChangeReferenceDirection || !_useLockDirectionByCameraOnSwitch)
                 {
-                    forward = Vector3.forward;
-                    right = Vector3.right;
-                }
-                else
-                {
-                    forward = camForward.normalized;
-                    right = camRight.normalized;
+                    _canChangeReferenceDirection = false;
+
+                    Vector3 camForward = Camera.main.transform.forward;
+                    Vector3 camRight = Camera.main.transform.right;
+
+                    camForward.y = 0;
+                    camRight.y = 0;
+
+                    if (camForward.sqrMagnitude < 0.01f || camRight.sqrMagnitude < 0.01f)
+                    {
+                        _forward = Vector3.forward;
+                        _right = Vector3.right;
+                    }
+                    else
+                    {
+                        _forward = camForward.normalized;
+                        _right = camRight.normalized;
+                    }
                 }
                 break;
 
             case CONTROLS.WORLD:
-                forward = Vector3.forward;
-                right = Vector3.right;
+                _forward = Vector3.forward;
+                _right = Vector3.right;
                 break;
         }
 
-        direction += Input.GetAxisRaw("Horizontal") * right;
-        direction += Input.GetAxisRaw("Vertical") * forward;
+        direction += horizontalInput * _right;
+        direction += verticalInput * _forward;
         direction.Normalize();
 
         _rigidbody.velocity = direction * speed + Vector3.up * _rigidbody.velocity.y;
